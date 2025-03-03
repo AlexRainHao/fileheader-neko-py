64efed43ca737c64e68bacd716eb50fb6f816d9a
AlexRainHao <yuanyuhaoyyh@gmail.com>
2025-03-04 00:48:43 +0800

    refactor: use native date to format date field

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,22 @@
 import * as vscode from "vscode";
-import dayjs from "dayjs";
 
 const _headerTemplate = (author: string, version: string) => {
+	const date = new Date();
+
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+	const day = String(date.getDate()).padStart(2, "0");
+	const hours = String(date.getHours()).padStart(2, "0");
+	const minutes = String(date.getMinutes()).padStart(2, "0");
+	const seconds = String(date.getSeconds()).padStart(2, "0");
 
 	return `# -*- coding: utf-8 -*-
 
 """
 @Description: 🐈
 @Author: ${author}
-@Copyright ${dayjs().year()} - ${dayjs().year()}
-@Date: ${dayjs().format("YYYY-MM-DD HH:mm:ss")}
+@Copyright ${year} - ${year}
+@Date: ${year}-${month}-${day} ${hours}:${minutes}:${seconds}
 @Version: ${version}
 """
 
